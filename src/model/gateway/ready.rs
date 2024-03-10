c269b2af3bbc8bee12b985660c533a6e3361d7e8
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-03-10 20:33:56 +0100

    add players fields

--- src/model/gateway/ready.rs
+++ src/model/gateway/ready.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use uuid::Uuid;
+use crate::model::player::PlayerInfo;
 
 /// The ready event, fired when a new connection is established
 /// with the server.
@@ -8,5 +9,7 @@ pub struct Ready {
     /// Whether if the session was resumed or not.
     pub resumed: bool,
     /// The session id itself.
-    pub session: Uuid
+    pub session: Uuid,
+    /// Players present in this session, this field is `Some` only when resuming connections.
+    pub players: Option<Vec<PlayerInfo>>
 }
