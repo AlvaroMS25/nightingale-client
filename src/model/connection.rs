bf052c5c62b61b0988404d13c8da9ff325759b25
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-08-06 21:54:57 +0200

    restore connection

--- src/model/connection.rs
+++ src/model/connection.rs
@@ -14,3 +14,30 @@ pub struct ConnectionInfo {
     /// Token of the connection.
     pub token: String
 }
+
+#[derive(Default)]
+pub(crate) struct PartialConnectionInfo {
+    pub channel_id: Option<NonZeroU64>,
+    pub endpoint: Option<String>,
+    pub session_id: Option<String>,
+    pub token: Option<String>
+}
+
+impl PartialConnectionInfo {
+    pub fn complete(&self) -> bool {
+        self.endpoint.is_some()
+            && self.session_id.is_some()
+            && self.token.is_some()
+    }
+
+    pub fn into_info(self) -> ConnectionInfo {
+        assert!(self.complete());
+
+        ConnectionInfo {
+            channel_id: self.channel_id,
+            endpoint: self.endpoint.unwrap(),
+            session_id: self.session_id.unwrap(),
+            token: self.token.unwrap()
+        }
+    }
+}
